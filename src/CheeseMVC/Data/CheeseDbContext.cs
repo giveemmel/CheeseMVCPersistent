f373eeb70d0e6e5456cb3a9bbae4b2010bc2df39
GiveEmMel <melinthestl@gmail.com>
2017-05-09 16:57:43 -0500

    Updated through Part 2 and beginning of Part 3

--- src/CheeseMVC/Data/CheeseDbContext.cs
+++ src/CheeseMVC/Data/CheeseDbContext.cs
@@ -7,6 +7,10 @@ namespace CheeseMVC.Data
     {
         public DbSet<Cheese> Cheeses { get; set; }
 
+        public DbSet<CheeseCategory> Categories { get; set; }
+
+        public DbSet<Menu> Menu { get; set; }
+
         public CheeseDbContext(DbContextOptions<CheeseDbContext> options) 
             : base(options)
         { }
---
+++ src/CheeseMVC/Migrations/20170509031145_AddCategory.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CheeseMVC.Migrations
+{
+    public partial class AddCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.ID);
+                });
+
+            migrationBuilder.AddColumn<int>(
+                name: "CheeseCategoryID",
+                table: "Cheeses",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cheeses_CheeseCategoryID",
+                table: "Cheeses",
+                column: "CheeseCategoryID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cheeses_Categories_CheeseCategoryID",
+                table: "Cheeses",
+                column: "CheeseCategoryID",
+                principalTable: "Categories",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cheeses_Categories_CheeseCategoryID",
+                table: "Cheeses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cheeses_CheeseCategoryID",
+                table: "Cheeses");
+
+            migrationBuilder.DropColumn(
+                name: "CheeseCategoryID",
+                table: "Cheeses");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
